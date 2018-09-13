875c3600b3f1576da3a5af4a81f03d0eb7286833
Mayuresh Krishna <mayureshkrishna@gmail.com>
2018-09-13 12:37:05 -0400

    Initial Commit

---
+++ springboot-xmldsl/src/main/java/io/pivotal/camel/CamelApplication.java
@@ -0,0 +1,14 @@
+package io.pivotal.camel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+@ImportResource("classpath:camelContext.xml")
+public class CamelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelApplication.class, args);
+	}
+}
